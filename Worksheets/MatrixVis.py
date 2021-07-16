3362df5afabd418c62648e338da37ff56b296f89
Jason Middlebrook <MiddlebrookJF@gmail.com>
2021-07-15 21:46:48 -0400

    Fix MatrixVis, Create Matrices
    Simplified MatrixVis.py by utilizing pandas.read_csv rather than reading the csv version of the matrix manually. This made it easy to convert the DF to a numpy matrix which I could then use with plt.imshow to create the matrix visualization, also known as a difference distance matrix (DDM).

--- Worksheets/MatrixVis.py
+++ Worksheets/MatrixVis.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 import matplotlib as mpl
-mpl.use('Agg')
+# mpl.use('Agg')
 import mpl_toolkits.mplot3d
 from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
@@ -12,30 +12,29 @@ from operator import itemgetter
 from matplotlib.colors import BoundaryNorm
 
 def matrix_vis(matrixDF):
-
-    fig=plt.figure()
-    ax=fig.add_subplot(111)
-    ax.set_aspect('equal')
-    cmap = plt.get_cmap('PuOr')
-    # define the colormap
-    cmap = plt.get_cmap('PuOr')
-    # extract all colors from the .jet map
-    cmaplist = [cmap(i) for i in range(cmap.N)]
-    # create the new map
-    cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
-
     print("\n\nMatrix:")
     print(matrixDF)
-    matrix = np.matrix(np.array((matrixDF)))
+
+    matrix_range = (int(matrixDF.columns[0]), int(matrixDF.columns[-1]), int(matrixDF.columns[0]), int(matrixDF.columns[-1]))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)                               # means only one plot
+    ax.set_aspect('equal')
+    cmap = plt.get_cmap('PuOr')                             # define the colormap
+    cmaplist = [cmap(i) for i in range(cmap.N)]             # extract all colors from the .jet map
+    cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)  # create the new map
+
+    matrix = np.matrix(matrixDF)
     print(matrix)
 
     vm = max(abs(np.min(matrix)), abs(np.max(matrix)))
     norm = mpl.colors.Normalize(vmin=-vm, vmax=vm, clip=False)
 
-    plt.imshow(matrix, interpolation='none', cmap=cmap, norm=norm) #cmap=plt.cm.ocean)
+    plt.imshow(matrix, interpolation='none', cmap=cmap, norm=norm, extent=matrix_range) #cmap=plt.cm.ocean)
     plt.colorbar()
-    #plt.show()
-    plt.savefig(f"draft.png")
+    ax.set_title('Uncleaved Closed S: V2 Values in Matrix Plot')
+    plt.savefig(f"Vas-Data/Matrices/Matrix_{matrix_range[0]}-{matrix_range[1]}.png")
+    plt.show()
 
 def read_matrix(path):
     with open(path, 'r') as scanFile:
@@ -51,5 +50,6 @@ def read_matrix(path):
     return pd.DataFrame(protein_matrix, columns=lines[0][1: len(lines[0])])
 
 #Executed stuff below here
-protein_matrix = read_matrix("Vas-Data/Matrices/6zge_700-1100.csv")
-matrix_vis(protein_matrix)
+# protein_matrix = read_matrix('Vas-Data/Matrices/6zge_700-1100.csv')
+proteinDF = pd.read_csv('Vas-Data/Matrices/6zge_700-1100.csv', index_col=0)
+matrix_vis(proteinDF)
